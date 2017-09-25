1242262d7a040e3eeaabccfa338a13ed37f1d961
Arash Outadi <arash.outadi@ravelin.com>
2017-09-25 14:01:45 +0100

    Forgot to append rule

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 
 // Rule functions
 // Function signature should be func(mlpapi.Slot) bool
-
+// TODO: Ask Astrid how to create a list of these without doing the append below
 func filterAvailable(slot mlpapi.Slot) bool {
 	return slot.Attributes.Availabilities > 0
 }
@@ -31,17 +31,20 @@ func main() {
 	// Return only available slots
 	rules = append(rules, filterAvailable)
 	rules = append(rules, filterAfterTime)
+	rules = append(rules, filterBeforeTime)
 	rules = append(rules, filterExcludeWeekends)
 	pitches := []mlpapi.Pitch{
 		mlpapi.Pitch{
 			ID:   "34933",
 			Path: "three-corners-adventure-playground/football-5-a-side-34933",
 			City: "london",
+			Name: "Three Corners",
 		},
 		mlpapi.Pitch{
 			ID:   "32208",
 			Path: "finsbury-leisure-centre/football-5-a-side-32208",
 			City: "london",
+			Name: "Finsbury Leisure Centre",
 		},
 	}
 	robo := roborooney.NewRobo(pitches, rules)
