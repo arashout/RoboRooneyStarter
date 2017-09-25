03a1da574e526a6ff61ddf77f6b584d570122f54
Arash Outadi <arash.outadi@ravelin.com>
2017-09-25 14:14:07 +0100

    Made adding rules simpler

--- main.go
+++ main.go
@@ -7,32 +7,25 @@ import (
 
 //go:generate echo '{"apiToken":"","channelId":""}' > config.json
 
-// Rule functions
-// Function signature should be func(mlpapi.Slot) bool
-// TODO: Ask Astrid how to create a list of these without doing the append below
-func filterAvailable(slot mlpapi.Slot) bool {
-	return slot.Attributes.Availabilities > 0
-}
-func filterAfterTime(slot mlpapi.Slot) bool {
-	// Only show slots after 4pm
-	return slot.Attributes.Starts.Hour() > 16
-}
-func filterBeforeTime(slot mlpapi.Slot) bool {
-	// Only show slots before 8pm
-	return slot.Attributes.Starts.Hour() < 20
-}
-func filterExcludeWeekends(slot mlpapi.Slot) bool {
-	// Week starts at Sunday == index 0
-	return slot.Attributes.Starts.Weekday() != 6 && slot.Attributes.Starts.Weekday() != 0
-}
-
 func main() {
-	rules := make([]func(mlpapi.Slot) bool, 0)
-	// Return only available slots
-	rules = append(rules, filterAvailable)
-	rules = append(rules, filterAfterTime)
-	rules = append(rules, filterBeforeTime)
-	rules = append(rules, filterExcludeWeekends)
+	rules := []func(slot mlpapi.Slot) bool{
+		func(slot mlpapi.Slot) bool {
+			// Return only the available slots
+			return slot.Attributes.Availabilities > 0
+		},
+		func(slot mlpapi.Slot) bool {
+			// Only show slots after 4pm
+			return slot.Attributes.Starts.Hour() > 16
+		},
+		func(slot mlpapi.Slot) bool {
+			// Only show slots before 8pm
+			return slot.Attributes.Starts.Hour() < 20
+		},
+		func(slot mlpapi.Slot) bool {
+			// Week starts at Sunday == index 0
+			return slot.Attributes.Starts.Weekday() != 6 && slot.Attributes.Starts.Weekday() != 0
+		},
+	}
 	pitches := []mlpapi.Pitch{
 		mlpapi.Pitch{
 			ID:   "34933",
